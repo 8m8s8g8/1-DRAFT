369cf9ace4a5109b2034dbcfc1eba1f055d60d88
Sergey <31635516+8m8s8g8@users.noreply.github.com>
2022-11-23 12:31:02 +0300

    задачи 2,4,6,8

---
+++ Task_4/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+
+int a = new Random().Next(0,100), b = new Random().Next(0,100), c = new Random().Next(0,100);
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
+int max = 0;
+if(a > max) max = a;
+if(b > max) max = b;
+if(c > max) max = c;
+Console.Write("max =  "); 
+Console.WriteLine (max);
---
+++ Task_6/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+//4 -> да
+//-3 -> нет
+//7 -> нет
+int a = 4; 
+int b =-3; 
+int c = 7;
+double a1 = (a % 2); double b1 = (b % 2); double c1 = (c % 2);
+Console.WriteLine(a);
+if(a1==0) 
+Console.WriteLine("Четное");
+else   
+Console.WriteLine("Не четное");
+Console.WriteLine(b); 
+if(b1==0) 
+Console.WriteLine("Четное");
+else   
+Console.WriteLine("Не четное");
+Console.WriteLine(c);
+if(c1==0) 
+Console.WriteLine("Четное");
+else   
+Console.WriteLine("Не четное"); 
