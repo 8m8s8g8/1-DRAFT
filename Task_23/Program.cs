e03b953f1daedd9005e2170819cf54b99f022517
Sergey <31635516+8m8s8g8@users.noreply.github.com>
2022-12-06 01:14:38 +0300

    задачи 2,...,21,23 решены

---
+++ Task_23/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Введите число до которого будет создана таблица");
+int N = Convert.ToInt32(Console.ReadLine());
+while (N == 0)
+{
+    Console.WriteLine("Не верный ввод, Введите число не равное нулю");
+    N = Convert.ToInt32(Console.ReadLine());
+}
+if (N > 0)
+    for (int i = 1; i <= Math.Abs(N); i++)
+    {
+        Console.WriteLine($"Куб числа {i}: {Math.Pow(i, 3)}");
+    }
+else
+    for (int i = 1; i >= N; i--)
+    {
+        Console.WriteLine($"Куб числа {i}: {Math.Pow(i, 3)}");
+    }
