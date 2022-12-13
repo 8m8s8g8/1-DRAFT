ac1f0902f2d4dd743f6d382a04d7087672ca8fb1
Sergey <31635516+8m8s8g8@users.noreply.github.com>
2022-12-13 14:04:48 +0300

    задачи 2,...,47 решены

---
+++ Task_47/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+int m = 3;
+int n = 4;
+double[,] arr = new double[m, n];
+
+
+PrintArray(FillArray(arr));
+double[,] FillArray(double[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(-10, 10);
+        }
+
+    }
+    return arr;
+}
+
+void PrintArray(double[,] ar)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            int k = new Random().Next(1, 10);
+            Console.Write($"{Math.Round(ar[i, j] / k, 1)} {" | "}  ");
+        }
+        Console.WriteLine(" ");
+    }
+
+}
