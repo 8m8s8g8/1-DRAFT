f9e308f7624efd8b4eb37ee8d0c7cfbac04aa5c1
Sergey <31635516+8m8s8g8@users.noreply.github.com>
2022-12-15 11:32:11 +0300

    задачи 2,...,52 решены

---
+++ Task_52/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int m = new Random().Next(2, 10);
+int n = new Random().Next(2, 10);
+int[,] arr = new int[m, n];
+
+
+PrintArray(FillArray(arr));
+int[,] FillArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(1, 10);
+        }
+
+    }
+    return arr;
+
+}
+
+void PrintArray(int[,] arr2)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr2[i, j]} {" | "} ");
+        }
+        Console.WriteLine();
+    }
+
+}
+
+for (int j = 0; j < arr.GetLength(1); j++)
+{
+    double sum = 0;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        sum += arr[i, j];
+    }
+    Console.WriteLine($"В столбце {j+1} среднее арифметическое равно { Math.Round(sum / arr.GetLength(0), 1) } ");
+}
+
+
